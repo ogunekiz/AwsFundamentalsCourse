1770f5f1e4a13f6fa8525c12bfab1fa5ef2c3a65
Ogün EKİZ <ogunekiz@hotmail.com>
2026-02-08 22:00:21 +0300

    Added S3 service

---
+++ 4.S3/Customers.WebAPI/Repositories/CustomerService.cs
@@ -0,0 +1,47 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+
+namespace Customers.WebAPI.Repositories
+{
+	public class CustomerService(IAmazonS3 s3)
+	{
+		private readonly string bucketName = "oefilesystem";
+		public async Task<PutObjectResponse> UploadImageAsync(Guid id, IFormFile file)
+		{
+			var putObjectRequest = new PutObjectRequest
+			{
+				BucketName = bucketName,
+				Key = $"images/{id}",
+				InputStream = file.OpenReadStream(),
+				ContentType = file.ContentType,
+				Metadata =
+				{
+				["x-amaz-mete-originalname"] = file.FileName,
+				["x-amaz-mete-extension"] = Path.GetExtension(file.FileName)
+
+				}
+			};
+
+			return await s3.PutObjectAsync(putObjectRequest);
+		}
+		public async Task<GetObjectResponse> GetImageAsync(Guid id)
+		{
+			var getObjectRequest = new GetObjectRequest
+			{
+				BucketName = bucketName,
+				Key = $"images/{id}"
+			};
+			return await s3.GetObjectAsync(getObjectRequest);
+		}
+		public async Task<DeleteObjectResponse> DeleteImageAsync(Guid id)
+		{
+			var deleteObjectRequest = new DeleteObjectRequest
+			{
+				BucketName = bucketName,
+				Key = $"images/{id}"
+			};
+
+			return await s3.DeleteObjectAsync(deleteObjectRequest);
+		}
+	}
+}
---
+++ 4.S3/S3Playground/Program.cs
@@ -0,0 +1,36 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+using System.Text;
+
+var s3Client = new AmazonS3Client();
+
+#region Upload
+//using var inputStream = new FileStream("D:/Projects/NET Projects/Console/AwsFundamentalsCourse/4.S3/S3Playground/bmw.jpg", FileMode.Open, FileAccess.Read);
+
+//var putObjectRequest = new PutObjectRequest
+//{
+//	BucketName = "oefilesystem",
+//	Key = "images/car.jpg",
+//	ContentType = "image/jpeg",
+//	InputStream = inputStream
+//};
+
+//await s3Client.PutObjectAsync(putObjectRequest);
+#endregion
+
+#region Download
+var getObjectRequest = new GetObjectRequest
+{
+	BucketName = "oefilesystem",
+	Key = "images/car.jpg"
+};
+
+var response = await s3Client.GetObjectAsync(getObjectRequest);
+
+using var memoryStream = new MemoryStream();
+response.ResponseStream.CopyTo(memoryStream);
+
+var text = Encoding.Default.GetString(memoryStream.ToArray());
+Console.WriteLine(text);
+
+#endregion
